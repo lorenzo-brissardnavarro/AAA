2b911152943ddbee1539acf204e3af853c358d51
florian <florian.gelin@laplateforme.io>
2025-12-09 10:09:56 +0100

    info proceseur

--- monitor.py
+++ monitor.py
@@ -3,7 +3,9 @@ import psutil
 
 cores_count = psutil.cpu_count()
 
+cores_freq = psutil.cpu_freq() 
 
+cores_usage_percentage = psutil.cpu_percent(1)
 
 
 # Lecture du fichier HTML pour remplacer les variables
@@ -12,6 +14,8 @@ html = open("template.html").read()
 
 # Remplacement des variables par les valeurs
 html = html.replace("{{ cores_count }}", str(cores_count))
+html = html.replace("{{ cores_freq }}", str(cores_freq))
+html = html.replace("{{ cores_usage_percentage}}", str(cores_usage_percentage))
 
 
 # Création du fichier contenant les variables remplacées
